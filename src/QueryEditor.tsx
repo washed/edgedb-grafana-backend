2b8a0268b0792c3db96bb13a9f8f18c08580011e
Marc Freudenberg <marc.freudenberg@mobilityhouse.com>
2022-09-20 17:36:38 +0200

    tiny frontend cleanup

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -9,18 +9,8 @@ import { defaultQuery, EdgeDBDataSourceOptions, EdgeDBQuery } from './types';
 type Props = QueryEditorProps<DataSource, EdgeDBQuery, EdgeDBDataSourceOptions>;
 
 export class QueryEditor extends PureComponent<Props> {
-  /*
-  onQueryTextChange = (event) => {
-    const { onChange, query, onRunQuery } = this.props;
-    console.log(`Event: ${event}`);
-    onChange({ ...query, queryText: event.target.value});
-    onRunQuery();
-  };
-  */
-
   onQueryTextChange: React.FormEventHandler<HTMLTextAreaElement> = (event) => {
     const { onChange, query, onRunQuery } = this.props;
-    console.log(`Event: ${event}`);
     onChange({ ...query, queryText: event.currentTarget.value});
     onRunQuery();
   }
@@ -35,7 +25,7 @@ export class QueryEditor extends PureComponent<Props> {
           onChange={this.onQueryTextChange}
           width={32}
           value={queryText || ''}
-          label="Query Text 2"
+          label="Query"
           type="string"
           readOnly={false}
           contentEditable={true}
