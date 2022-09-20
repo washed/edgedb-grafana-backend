44f191df4683526c3d380569bc29e1b7d3427d44
Marc Freudenberg <marc.freudenberg@mobilityhouse.com>
2022-09-20 21:43:16 +0200

    add editor font

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -11,27 +11,33 @@ type Props = QueryEditorProps<DataSource, EdgeDBQuery, EdgeDBDataSourceOptions>;
 export class QueryEditor extends PureComponent<Props> {
   onQueryTextChange: React.FormEventHandler<HTMLTextAreaElement> = (event) => {
     const { onChange, query, onRunQuery } = this.props;
-    onChange({ ...query, queryText: event.currentTarget.value});
+    onChange({ ...query, queryText: event.currentTarget.value });
     onRunQuery();
-  }
+  };
 
   render() {
     const query = defaults(this.props.query, defaultQuery);
     const { queryText } = query;
 
+    const editor_style = {
+      fontFamily: 'Roboto Mono,monospace',
+      fontSize: '14px',
+    };
+
     return (
       <div className="gf-form">
         <TextArea
           onChange={this.onQueryTextChange}
-          width={32}
+          rows={12}
+          cols={32}
           value={queryText || ''}
           label="Query"
           type="string"
           readOnly={false}
           contentEditable={true}
+          style={editor_style}
         />
       </div>
     );
   }
-
 }
